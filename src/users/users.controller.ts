991f6283da3947c486b09a0953949bc5fcb2eff4
Riki Afila F <rikiafilaf.work@gmail.com>
2024-05-18 13:42:22 +0700

    Tambah route findAll dan findOne User

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get('/:id')
+  findOne(@Param('id') id: number) {
+    return this.usersService.findOne(id);
+  }
 
   @Post()
   createUser(@Body() body: CreateUserDto) {
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,8 +9,16 @@ export class UsersService {
     @InjectRepository(User) private usersRepository: Repository<User>,
   ) {}
 
+  findAll() {
+    return this.usersRepository.find();
+  }
+
   create(name: string, email: string, password: string) {
     const user = this.usersRepository.create({ name, email, password });
     return this.usersRepository.save(user);
+  }
+
+  findOne(id: number) {
+    return this.usersRepository.findOneBy({id});
   }
 }
