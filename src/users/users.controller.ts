481eaaaf785670602eb85a324879a9f8b1930511
Riki Afila F <rikiafilaf.work@gmail.com>
2024-05-18 09:46:10 +0700

    Tambah proses save data user ke db

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post()
   createUser(@Body() body: CreateUserDto) {
-    return body;
+    this.usersService.create(body.name, body.email, body.password);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+
+  create(name: string, email: string, password: string) {
+    const user = this.usersRepository.create({ name, email, password });
+    return this.usersRepository.save(user);
+  }
+}
