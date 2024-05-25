49bd0fd3981d0e5808b5087c69c3650f32dbbd5d
Riki Afila F <rikiafilaf.work@gmail.com>
2024-05-25 21:49:25 +0700

    Implementasi penggunaan auth guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,15 +7,18 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('users')
 @Serialize(UserDto)
+@UseGuards(AuthGuard)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
