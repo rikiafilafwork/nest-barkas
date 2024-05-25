1cdecec598d3afe3de5677e282b161e30657cf0e
Riki Afila F <rikiafilaf.work@gmail.com>
2024-05-25 14:23:53 +0700

    Tambah method auth login user

--- src/users/auth/auth.service.ts
+++ src/users/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { UsersService } from '../users.service';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
@@ -19,6 +19,22 @@ export class AuthService {
     const hashedPassword = salt + '.' + hash.toString('hex');
 
     const user = await this.usersService.create(name, email, hashedPassword);
+
+    return user;
+  }
+
+  async login(email: string, password: string) {
+    const [user] = await this.usersService.findAll(email);
+    if(!user) {
+      throw new NotFoundException('Email tidak terdaftar');
+    }
+
+    const [salt, storedHash] = user.password.split('.')
+
+    const hash = (await scrypt(password, salt, 64)) as Buffer;
+    if(storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('Password salah');
+    }
 
     return user;
   }
---
+++ src/users/dtos/login-user.dto.ts
@@ -0,0 +1,7 @@
+import { IsEmail, IsString } from 'class-validator';
+export class LoginUserDto {
+  @IsEmail()
+  email: string;
+  @IsString()
+  password: string;
+}
