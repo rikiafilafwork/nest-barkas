c784030dda7170dd2ac828cec5fc6bb5c06bae9d
Riki Afila F <rikiafilaf.work@gmail.com>
2024-06-01 20:03:43 +0700

    Create Auth service test unit

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
+import { User } from '../users/user.entity';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
+    const fakeUsersService: Partial<UsersService> = {
+      findAll: () => Promise.resolve([]),
+      create: (name: string, email: string, password: string) => {
+        return Promise.resolve({ id: 1, name, email, password } as User)
+      }
+    }
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [AuthService, {
+        provide: UsersService,
+        useValue: fakeUsersService
+      }],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -15,4 +26,15 @@ describe('AuthService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should create user', async () => {
+    const user = await service.register('foo', 'bar@gmail.com', 'baz');
+    expect(user.password).not.toEqual('baz');
+
+    expect(user.name).toBe('foo');
+    expect(user.email).toBe('bar@gmail.com');
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  })
 });
